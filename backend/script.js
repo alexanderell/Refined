620f00d21ce5d26a46adb5d6a68646d496643e0e
Alexander <alexander.ellnestam@gmail.com>
2025-06-29 15:35:24 +0200

    Added translation for book now question

--- backend/script.js
+++ backend/script.js
@@ -11,6 +11,8 @@ const translations = {
         products: "Produkter",
         howTo: "Förberedelser",
         faq: "FAQ",
+        bookFaq: "Hur bokar jag en tid?",
+        bookText: "Du kan boka din tid här.",
         aboutUsText: "Mitt namn är Linnea, Jag har arbetat med naglar, lash & browlift sedan jag blev certifierad 2023. Jag kan stolt säga att jag idag är Master utbildad i lashlift och browlift. Du är alltid mitt största fokus, var inte rädd för att fråga eller be om något! Jag brinner verkligen för detta yrke och strävar alltid efter allt lära mig något nytt. Jag håller mig uppdaterad inom trendiga stilar och tekniker för att alltid ge kunden det lilla extra. Mitt mål är att erbjuda fler tjänster och i framtiden lära ut andra i detta fantastiska yrke, är så tacksam och stolt över det jag åstadkommit hittills. Jag är utbildad inom naglar från Semilac och utbildad i lash & browlift av Vaidas lash academy. Master utbildad av Permalift. Hitta mig på sociala medier och spana in mitt arbete i sektionen 'Kontakta oss'!"
     },
     en: {
@@ -25,6 +27,8 @@ const translations = {
         products: "Products",
         howTo: "How To's",
         faq: "FAQ",
+        bookFaq: "How do I book an apointment?",
+        bookText: "You can book an appointment here.",
         aboutUsText: "My name is Linnea. I have been working with nails, lash & brow lifts since I became certified in 2023. I can proudly say that I am now Master-certified in lash lifts and brow lifts. You are always my top priority – don’t hesitate to ask questions or request anything! I’m truly passionate about this profession and always strive to learn something new. I stay up to date with trendy styles and techniques to always give my clients that little extra. My goal is to offer more services and eventually teach others in this amazing profession. I am so grateful and proud of what I have accomplished so far. I am trained in nails by Semilac and certified in lash & brow lifts by Vaida’s Lash Academy. I received my Master certification from Permalift. Find me on social media and check out my work in the 'contact us' section!"
 
     },
